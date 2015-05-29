8fa3058fbcbb370758e304ce1d8e9c18e054e224
Wenting Wang <wwang27@L-SB8J25MG3Q-M.homeoffice.wal-mart.com>
2015-05-28 17:49:01 -0700

    add start/stop script and setup.conf. Start datanode has a problem that some data nodes can not join the cluster. Stop.sh need to remove sudo rm -r /data1/datanode

---
+++ stop.sh
@@ -0,0 +1,47 @@
+#! /bin/bash
+
+############################## PRE PROCESSING ################################
+#check and process arguments
+REQUIRED_NUMBER_OF_ARGUMENTS=2
+if [ $# -lt $REQUIRED_NUMBER_OF_ARGUMENTS ]
+then
+echo "Usage: $0 <path_to_config_file> <type_of_start> "
+echo "Type of start: namenode or datanode "
+exit 1
+fi
+
+CONFIG_FILE=$2
+
+echo "Config file is $CONFIG_FILE"
+echo ""
+source $CONFIG_FILE
+
+##############################STOP NAME/DATA NODE##############################
+
+if [ "$1" == "namenode" ]
+then
+    COMMAND="sudo $HADOOP_HOME/sbin/stop-dfs.sh"
+    for node in ${NAME_NODE//,/ }
+    do
+        echo "Stop name node in $node in $COMMAND"
+        ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $node "
+        $COMMAND"
+        break
+    done
+
+elif [ "$1" == "datanode" ]
+then
+    COMMAND="sudo HADOOP_SECURE_DN_USER=$USER $HADOOP_HOME/sbin/hadoop-daemon.sh --config $HADOOP_HOME/etc/hadoop --script hdfs stop datanode;sudo rm -r /data1/datanode"
+    for node in ${DATA_NODE//,/ }
+    do
+        echo "Stop name node in $node in sudo $COMMAND"
+        ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $node "
+        $COMMAND"
+    done
+else
+    echo "Unrecongized start type: namenode or datanode"
+    exit 1
+fi
+
+
+
